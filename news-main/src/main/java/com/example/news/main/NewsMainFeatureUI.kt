6ffba6a52020569c595c3f1f004144b85bf87978
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-26 00:38:30 +0500

    video-4 finish adding image

--- news-main/src/main/java/com/example/news/main/NewsMainFeatureUI.kt
+++ news-main/src/main/java/com/example/news/main/NewsMainFeatureUI.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,17 +17,21 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import com.example.news.uikit.NewsTheme
 
 @Composable
@@ -113,17 +119,29 @@ private fun ProgressIndicator(state: State.Loading){
  internal fun Article(
     @PreviewParameter(ArticlePreviewProvider::class, limit = 1)article: ArticleUI,
  ) {
-     Row {
+     Row(Modifier.padding(bottom = 4.dp)) {
          article.imageUrl?.let {imageUrl ->
-             AsyncImage(
-                 model = imageUrl,
-                 contentDescription = stringResource(R.string.content_desc_item_article_image)
-             )
+             var isImageVisible by remember {
+                 mutableStateOf(true)
+             }
+             if(isImageVisible){
+                 AsyncImage(
+                     model = imageUrl,
+                     onState = {state ->
+                               if(state is AsyncImagePainter.State.Error){
+                                   isImageVisible = false
+                               }
+                     },
+                     contentDescription = stringResource(R.string.content_desc_item_article_image),
+                     contentScale = ContentScale.Crop,
+                     modifier = Modifier.size(150.dp)
+                 )
+             }
          }
          Spacer(modifier = Modifier.size(4.dp))
          Column(modifier = Modifier.padding(8.dp)){
              Text(
-                 text = article.title ?: "No TITLE",
+                 text = article.title,
                  style = NewsTheme.typography.headlineMedium,
                  maxLines = 1)
              Spacer(modifier = Modifier.size(4.dp))
