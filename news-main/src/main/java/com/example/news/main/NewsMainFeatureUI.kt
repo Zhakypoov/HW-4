531f7829d10b70b8ec4745de65b4e16771fd4362
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-25 23:39:59 +0500

    video-4 add image

--- news-main/src/main/java/com/example/news/main/NewsMainFeatureUI.kt
+++ news-main/src/main/java/com/example/news/main/NewsMainFeatureUI.kt
@@ -3,6 +3,7 @@ package com.example.news.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,14 +15,17 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.news.uikit.NewsTheme
 
 @Composable
@@ -109,14 +113,23 @@ private fun ProgressIndicator(state: State.Loading){
  internal fun Article(
     @PreviewParameter(ArticlePreviewProvider::class, limit = 1)article: ArticleUI,
  ) {
-Column(modifier = Modifier.padding(8.dp)){
-    Text(
-        text = article.title ?: "No TITLE",
-        style = NewsTheme.typography.headlineMedium,
-        maxLines = 1)
-       Spacer(modifier = Modifier.size(4.dp))
-       Text(text = article.description, style = NewsTheme.typography.bodyMedium, maxLines = 3)
-   }
+     Row {
+         article.imageUrl?.let {imageUrl ->
+             AsyncImage(
+                 model = imageUrl,
+                 contentDescription = stringResource(R.string.content_desc_item_article_image)
+             )
+         }
+         Spacer(modifier = Modifier.size(4.dp))
+         Column(modifier = Modifier.padding(8.dp)){
+             Text(
+                 text = article.title ?: "No TITLE",
+                 style = NewsTheme.typography.headlineMedium,
+                 maxLines = 1)
+             Spacer(modifier = Modifier.size(4.dp))
+             Text(text = article.description, style = NewsTheme.typography.bodyMedium, maxLines = 3)
+         }
+     }
 }
 
 
